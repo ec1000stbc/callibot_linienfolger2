e97eb8122961a379781ed80f51e1fb8201bfe588
ec1000stbc <ch.raba@gmx.de>
2026-02-14 18:12:45 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let speed = 30
 let right = speed
 right = speed - 2
 let on = 0
-let corr = 10
+let corr = 12
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
     if (input.buttonIsPressed(Button.A)) {
@@ -19,7 +19,7 @@ basic.forever(function () {
         while (maqueen.readPatrol(maqueen.Patrol.PatrolLeft, maqueen.Brightness.Dark) && maqueen.readPatrol(maqueen.Patrol.PatrolRight, maqueen.Brightness.Bright)) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed - corr)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed + corr)
-            basic.pause(100)
+            basic.pause(80)
         }
         while (maqueen.readPatrol(maqueen.Patrol.PatrolRight, maqueen.Brightness.Dark) && maqueen.readPatrol(maqueen.Patrol.PatrolLeft, maqueen.Brightness.Bright)) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed - corr)
